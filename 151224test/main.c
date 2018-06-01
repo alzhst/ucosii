ffcb227a53062901eaf20103536108c4e7fc7d16
alzhst <sun_tongtong@foxmail.com>
2018-06-02 01:37:26 +0800

    新增练习3-10-15代码，存在问题，需验证

--- 151224test/main.c
+++ 151224test/main.c
@@ -1,44 +1,59 @@
 #include "SystemConfig.h"
 
-/*定义堆栈*/
-OS_STK MainTask_Stk[MainTask_StkSize];
-OS_STK App1Task_Stk[AppTask_StkSize];
-OS_STK App2Task_Stk[AppTask_StkSize];
-OS_STK App3Task_Stk[AppTask_StkSize];
+OS_STK MainTaskStk[MainTask_StkSize];
+OS_STK App1TaskStk[AppTask_StkSize];
+OS_STK App2TaskStk[AppTask_StkSize];
+OS_STK App3TaskStk[AppTask_StkSize];
+
+
 
 int main(void)
 {
-	OSInit(); /*系统初始化*/
+	OSInit();
 
-	OSTaskCreate(MainTask, (void*)0, &MainTask_Stk[MainTask_StkSize-1], MainTask_Prio);/*创建主任务*/
+	OSTaskCreate(MainTask, (void*)0,&MainTaskStk[MainTask_StkSize-1],MainTask_Prio);
 
-	OSStart();/*系统启动*/
-	return 0;
+	OSStart();
 }
 
-void MainTask(void *p_arg)
+
+void MainTask(void * p_arg)
 {
 	p_arg = p_arg;
-	OSTaskCreate(App1Task, (void*)0, &App1Task_Stk[AppTask_StkSize-1], App1Task_Prio);/*分别创建三个任务*/
-	OSTaskCreate(App2Task, (void*)0, &App2Task_Stk[AppTask_StkSize-1], App2Task_Prio);
-	OSTaskCreate(App3Task, (void*)0, &App3Task_Stk[AppTask_StkSize-1], App3Task_Prio);
+	OSTaskCreate(App1Task, (void*)0, &App1TaskStk[AppTask_StkSize-1],App1Task_Prio);
+	OSTaskCreate(App2Task, (void*)0, &App2TaskStk[AppTask_StkSize-1],App2Task_Prio);
+	OSTaskCreate(App3Task, (void*)0, &App3TaskStk[AppTask_StkSize-1],App3Task_Prio);
 	while(1)
 	{
-		OSTimeDlyHMSM(0, 0, 1, 0);
+		OSTimeDlyHMSM(0,0,1,0);
 	}
-	return;
 }
-
 
 void App1Task(void * p_arg)
 {
 	p_arg = p_arg;
+	INT32U ticks;
 	while(1)
 	{
+		ticks = OSTimeGet();
+		printf("ticks: %d\n",ticks);
+		if(ticks > 1000 && ticks < 1500)
+		{
+			while(OSTaskDelReq(App2Task_Prio) != OS_ERR_TASK_NOT_EXIST)
+			{
+				OSTimeDly(1);
+			}
+		}
+		else if(ticks > 2000 && ticks < 3500)
+		{
+			while(OSTaskDelReq(App3Task_Prio) != OS_ERR_TASK_NOT_EXIST)
+			{
+				OSTimeDly(1);
+			}
+		}
 		printf("1\n");
-		OSTimeDlyHMSM(0, 0, 3, 0);
+		OSTimeDlyHMSM(0,0,6,0);
 	}
-	
 }
 
 void App2Task(void * p_arg)
@@ -46,54 +61,37 @@ void App2Task(void * p_arg)
 	p_arg = p_arg;
 	while(1)
 	{
-		printf("2\n");
-		OSTimeDlyHMSM(0, 0, 6, 0);
-	}
-}
-void Show_OSTCB(OS_TCB * pdata)
-{
-	printf("OSTCB: %lu\n",(unsigned long)pdata);
-	printf("OSTCBPrev: %lu\n", (unsigned long)pdata->OSTCBPrev);
-	printf("OSTCBNext: %lu\n", (unsigned long)pdata->OSTCBNext);
-	printf("OSTCBDly: %ul\n", pdata->OSTCBDly);
-	printf("OSTCBStat: %u\n", pdata->OSTCBStat);
-	printf("OSTCBStatPend: %u\n", pdata->OSTCBStatPend);
-	printf("OSTCBPrio: %u\n", pdata->OSTCBPrio);
-	printf("OSTCBX: %u\n", pdata->OSTCBX);
-	printf("OSTCBY: %u\n", pdata->OSTCBY);
-	printf("OSTCBBitX: %u\n", pdata->OSTCBBitX);
-	printf("OSTCBBitY: %u\n", pdata->OSTCBBitY);
-}
-void App3Task(void * p_arg)
-{
-	OS_TCB MainPdata;
-	OS_TCB App1Pdate;
-	OS_TCB App2Pdata;
-	p_arg = p_arg;
-	while(1)
-	{
-		printf("3\n");
-		if(OSTaskQuery(MainTask_Prio, &MainPdata) == OS_ERR_NONE)
+		if(OSTaskDelReq(OS_PRIO_SELF) == OS_ERR_TASK_DEL_REQ)
 		{
-			printf("Main pdata\n");
-			Show_OSTCB(&MainPdata);
+			printf("AppTask1:删除AppTask2\n");
+			OSTaskDel(OS_PRIO_SELF);
 		}
-		if(OSTaskQuery(App1Task_Prio, &App1Pdate) == OS_ERR_NONE)
+		else
 		{
-			printf("App1 pdata\n");
-			Show_OSTCB(&App1Pdate);
+			printf("2\n");
+			OSTimeDlyHMSM(0,0,1,0);
 		}
-		if(OSTaskQuery(App2Task_Prio, &App2Pdata) == OS_ERR_NONE)
-		{
-			printf("App2 pdata\n");
-			Show_OSTCB(&App2Pdata);
-		}
-		printf("App3 pdata\n");
-		Show_OSTCB(OSTCBCur);
-		OSTimeDlyHMSM(0, 0, 9, 0);
 	}
 }
 
+
+void App3Task(void * p_arg)
+{
+	p_arg = p_arg;
+	while(1)
+	{
+		if(OSTaskDelReq(OS_PRIO_SELF) == OS_ERR_TASK_DEL_REQ)
+		{
+			printf("AppTask1:删除AppTask3\n");
+			OSTaskDel(OS_PRIO_SELF);
+		}
+		else
+		{
+			printf("3\n");
+			OSTimeDlyHMSM(0,0,2,0);
+		}
+	}
+}
 
 
 
